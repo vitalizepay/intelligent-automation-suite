b899a634e360c9ca502ee2fc59d14a41f85fca74
vitalizepay <vitalizepay@gmail.com>
2026-04-28 15:04:34 +0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-
+ 
 export default tseslint.config(
   { ignores: ["dist", ".output", ".vinxi"] },
   {
